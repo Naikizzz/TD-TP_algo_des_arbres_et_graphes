a75d2a6dcf2b426f8406ec79e8c6f39e5e5cfaca
Naikizzz <ahunaiki.otto@gmail.com>
2025-02-23 10:03:22 -1000

    Modification + ajout
    Changement du constructeur d'arbre de recherche mutable vide et ajout de la méthode "insert" dans la classe SMTree

--- smtree.py
+++ smtree.py
@@ -7,8 +7,17 @@ Created on Thu Feb 20 15:48:10 2025
 
 from binary_tree import *
 
-class SMEmpty(MEmpty):
-    """Constructeur pour un arbre vide de recherche mutable"""
-
-class SMTree(MTree,MNode):
-        
+class SMTree(MTree):
+    
+    def empty(self):
+        self.set_empty()
+    
+    def insert(self, x):
+        if self.empty():
+            return self.set_data(x)
+        elif(x < self.data()):
+            return self.left_son.insert(x)
+        elif(x > self.data()):
+            return self.right_son.insert(x)
+        else:
+            return self
