c3e386c2dd09773367d6ecb470de2e4a2617f9ed
Naikizzz <ahunaiki.otto@gmail.com>
2025-02-24 13:49:41 -1000

    Modification
    Changement de méthode de création de la classe en l'intégrant dans une fonction MakeSMTree. Modification de insert qui modifie en place.

--- smtree.py
+++ smtree.py
@@ -7,28 +7,36 @@ Created on Thu Feb 20 15:48:10 2025
 
 from binary_tree import *
 
-class SMTree(MTree):
-    
-    def empty(self):
+def MakeSMTree(key=lambda x:x, val=lambda x:x):
+
+    class SMTree(MTree):
         
-        self.set_empty()
-    
-    def insert(self, x):
+        def empty(self):
+            
+            self.set_empty()
         
-        if self.empty():
-            return self.set_data(x)
-        elif(x < self.data()):
-            return self.left_son().insert(x)
-        elif(x > self.data()):
-            return self.right_son().insert(x)
+        def insert(self, x):
+            
+            if self.empty():
+                self.set_data(x)
+            elif(x < self.data()):
+                self.left_son().insert(x)
+            elif(x > self.data()):
+                self.right_son().insert(x)
+            
+        def mem(self, x):
+            
+            if self.empty():
+                return False
+            if x == self.data():
+                return True
+            elif x < self.data():
+                return self.left_son().mem(x)
+            else:
+                return self.right_son().mem(x)
         
-    def mem(self, x):
+        def remove(self, x):
+            ...
         
-        if self.empty():
-            return False
-        if x == self.data():
-            return True
-        elif x < self.data():
-            return self.left_son().mem(x)
-        else:
-            return self.right_son().mem(x)
+        def check():
+            ...
