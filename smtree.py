c3de1f8db916e181ba0685098d0e29bad8379ee3
Naikizzz <ahunaiki.otto@gmail.com>
2025-02-24 14:45:03 -1000

    Remplissage
    Remplissage de la fonction _replace et complétion de la fonction pop_max.

--- smtree.py
+++ smtree.py
@@ -45,13 +45,20 @@ def MakeSMTree(key=lambda x:x, val=lambda x:x):
                 if self.left_son().empty():
                     self.set_empty()
                 else:
-                    ...
+                    self._replace(self.left_son())
+                return max_val
+            else:
+                return self.right_son().pop_max()
         
         def _replace(self, node):
             """
             Méthode permettant de remplacer
             le noeud courant par un noeud donné
             """
+            self.set_data(node.data())
+            self.set_left_son(node.left_son())
+            self.set_right_son(node.right_son())
+            
         
         def remove(self, x):
             ...
